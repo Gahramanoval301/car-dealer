71bb45dc6ef24c94707971f9aa47ea88d53fdd28
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-01-09 00:15:18 +0400

    flex wrap

--- src/components/_home/Filter.tsx
+++ src/components/_home/Filter.tsx
@@ -12,7 +12,7 @@ const Filter = ({ Results }: IVehicleResponseData) => {
   const isLinkDisabled = !selectedValues.name  || !selectedValues.year;
   
   return (
-    <div className='shadow-sm flex justify-evenly items-center p-5 bg-bluelight rounded-xl '>
+    <div className='shadow-sm flex justify-evenly items-center p-5 bg-bluelight rounded-xl flex-wrap sm:flex-nowrap gap-10'>
       <SelectForm setSelectedValues={setSelectedValues} selectedValues={selectedValues} type="name" results={Results} />
       <SelectForm setSelectedValues={setSelectedValues} selectedValues={selectedValues} type="years" results={Results} />
       <Link href={`/result/${selectedValues.name}/${selectedValues.year}`}  className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ${
