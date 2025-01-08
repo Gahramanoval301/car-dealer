fc56f967392e7f7cff39e6fac2516a75a16e19ec
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-01-08 23:40:12 +0400

    add icon for selection

--- src/components/Common/Select.tsx
+++ src/components/Common/Select.tsx
@@ -3,15 +3,15 @@
 import { years } from "@/data/selectedYears";
 import { IVehicle } from "../../../api/vehicles";
 import { ChangeEvent } from "react";
-
+import { IoIosArrowDropdown } from "react-icons/io";
 interface ISelectedValues {
     name: string;
     year: string;
 }
 interface ISelectForm {
-    setSelectedValues: (values: ISelectedValues) => void; 
+    setSelectedValues: (values: ISelectedValues) => void;
     selectedValues: ISelectedValues;
-    type: "name" | "years"; 
+    type: "name" | "years";
     results: IVehicle[] | null;
 }
 export default function SelectForm({ setSelectedValues, selectedValues, type, results }: ISelectForm) {
@@ -21,7 +21,7 @@ export default function SelectForm({ setSelectedValues, selectedValues, type, re
             setSelectedValues({ ...selectedValues, name: event.target.value });
         }
         if (type === "years") {
-            setSelectedValues({ ...selectedValues, year:event.target.value });  
+            setSelectedValues({ ...selectedValues, year: event.target.value });
         }
     };
 
@@ -29,41 +29,48 @@ export default function SelectForm({ setSelectedValues, selectedValues, type, re
         <form className="max-w-sm mx-auto">
 
             {type == "name" ?
+                <div className="relative">
+                    <select
+                        id="make_name_select"
+                        value={selectedValues.name}
+                        onChange={handleChange}
+                        className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                    >
 
-                <select
-                    id="make_name_select"
-                    value={selectedValues.name}
-                    onChange={handleChange}
-                    className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-                >
+                        {results?.map((result: IVehicle) => (
+                            <option key={result.MakeId} value={result.MakeId} className="px-1">
+                                {result.MakeName}
+                            </option>
+                        ))
+                        }
 
-                    {results?.map((result: IVehicle) => (
-                        <option key={result.MakeId} value={result.MakeId} className="px-1">
-                            {result.MakeName}
-                        </option>
-                    ))
-                    }
-
-                </select>
+                    </select>
+                    <span className="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none">
+                        <IoIosArrowDropdown />
+                    </span>
+                </div>
                 :
-                <select
-                id="year_select"
-                value={selectedValues.year}
-                onChange={handleChange}
-                className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-            >
+                <div className="relative">
+                    <select
+                        id="year_select"
+                        value={selectedValues.year}
+                        onChange={handleChange}
+                        className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                    >
 
 
 
-                {years?.map((year: number) => (
-                    <option key={year} value={year} className="px-1">
-                        {year}
-                    </option>
-                ))
-                }
-
-
-            </select>
+                        {years?.map((year: number) => (
+                            <option key={year} value={year} className="px-1">
+                                {year}
+                            </option>
+                        ))
+                        }
+                        <span className="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none">
+                            <IoIosArrowDropdown />
+                        </span>
+                    </select>
+                </div>
             }
         </form>
     );
